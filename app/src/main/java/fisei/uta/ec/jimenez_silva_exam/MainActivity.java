32e77ee9f1cd0d5af508d415676f7bbbe15b2843
BrunoJimenezB <105023940+BrunoJimenezB@users.noreply.github.com>
2022-05-30 18:37:29 -0500

    primer commit

---
+++ app/src/main/java/fisei/uta/ec/jimenez_silva_exam/MainActivity.java
@@ -0,0 +1,23 @@
+package fisei.uta.ec.jimenez_silva_exam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+private Button buttonSiguiente;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        buttonSiguiente= findViewById(R.id.buttonSiguiente1);
+    }
+    public void onClickSiguiente(View view){
+        Intent intent = new Intent(MainActivity.this, MainActivity2J_S.class);
+        startActivity(intent);
+    }
+
+}
---
+++ app/src/main/java/fisei/uta/ec/jimenez_silva_exam/MainActivity2J_S.java
@@ -0,0 +1,14 @@
+package fisei.uta.ec.jimenez_silva_exam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2J_S extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity2_js);
+    }
+}
